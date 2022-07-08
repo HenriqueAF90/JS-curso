e703516b57d8afdc84f0e12afcab53f58e819341
Henrique Andrade <henriqueandrade13@hotmail.com>
2022-07-07 23:23:19 -0300

    envio do exercicio 1

---
+++ Exercicios/ex_001.js
@@ -0,0 +1,7 @@
+var area = 2.5;
+var altura = 1.75;
+
+console.log('Calcule quanto de tinta para pintura em paredes');
+
+console.log('Sua área é de ' + (area * altura) + 'm²');
+console.log('Para pintar essa parede você vai precisar de ' +(area * altura)/2 + ' litros de tinas')
